ebb735614af1e72d80bd0a0bf3b73b5abd622686
Ivan Serdonintsev <ivan.serdobintseff@yandex.ru>
2026-02-26 18:24:48 +0300

    my first git

---
+++ sem02/01.cpp
@@ -0,0 +1,7 @@
+#include <iostream>             // "import" of library
+
+int main() {                    // all cpp programs have main function, it is our entry point
+  std::cout << "Hello, world!"  // Special function to put (<<) something in console
+    << std::endl;               // Special function to set new line
+  return 0;                     // return 0 to say "everything is fine"
+}
